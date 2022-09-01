58d846dad91502f512a4d1aec55e899ae2d889b4
Jeremy <mail.jeremy.li@gmail.com>
2022-09-01 20:06:21 +1000

    http service test

--- src/app/shared/services/followers/followers.service.spec.ts
+++ src/app/shared/services/followers/followers.service.spec.ts
@@ -1,16 +1,76 @@
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { asyncData } from 'src/app/utils/async-data';
+import { asyncError } from 'src/app/utils/async-error';
 
 import { FollowersService } from './followers.service';
+import { mockFollowerResponse } from './mock-follower-response';
 
-// describe('FollowersService', () => {
-  // let service: FollowersService;
+describe('FollowersService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let service: FollowersService;
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(FollowersService);
-  // });
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
-// });
+    // either A:
+    // TestBed.configureTestingModule({
+    //   imports: [
+    //     HttpClientModule,
+    //   ],
+    //   providers: [
+    //     { provide: HttpClient, useValue: httpClientSpy },
+    //   ],
+    // });
+    // service = TestBed.inject(FollowersService);
+
+    // or B:
+    service = new FollowersService(httpClientSpy);
+  });
+
+  it('#fetchFollowers should return expected followers', (done: DoneFn) => {
+
+    httpClientSpy.get.and.returnValue(asyncData(mockFollowerResponse.data)); // may also use of(mockFollowerResponse.data) if we don't care to test it strictly being async
+
+    service.fetchFollowers().subscribe({
+      next: (followers) => {
+        expect(followers)
+          .withContext('expected followers')
+          .toEqual(mockFollowerResponse.data.results);
+        done();
+      },
+      error: done.fail,
+    });
+
+    expect(httpClientSpy.get.calls.count())
+      .withContext('one call')
+      .toBe(1);
+
+  });
+
+  it('#fetchFollowers should pass on the error', (done: DoneFn) => {
+
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404,
+      statusText: 'Not Found',
+    });
+
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse)); // may also use throwError(() => errorResponse) if we don't care to test it strictly being async
+
+    service.fetchFollowers().subscribe({
+      next: () => done.fail('should have got an error, not followers'),
+      error: (error) => {
+        expect(error)
+          .withContext('expected error')
+          .toEqual(errorResponse);
+        done();
+      },
+    });
+
+    expect(httpClientSpy.get.calls.count())
+      .withContext('one call')
+      .toBe(1);
+
+  });
+});
---
+++ src/app/shared/services/followers/mock-follower-response.ts
@@ -0,0 +1,66 @@
+export const mockFollowerResponse = {
+  data: {
+    results: [
+      {
+        name: {
+          first: 'Jeremy',
+          last: 'Aurelius',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/men/96.jpg',
+        },
+        login: {
+          username: 'jeremyaurelius',
+        },
+      },
+      {
+        name: {
+          first: 'Bastian',
+          last: 'Hallix',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/men/56.jpg',
+        },
+        login: {
+          username: 'bastianhallix',
+        },
+      },
+      {
+        name: {
+          first: 'Lyris',
+          last: 'Titanborn',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/women/97.jpg',
+        },
+        login: {
+          username: 'lyristitanborn',
+        },
+      },
+      {
+        name: {
+          first: 'Falk',
+          last: 'Firebeard',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/men/68.jpg',
+        },
+        login: {
+          username: 'falkfirebeard',
+        },
+      },
+      {
+        name: {
+          first: 'Elisif',
+          last: 'the Fair',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/women/97.jpg',
+        },
+        login: {
+          username: 'elisifthefair',
+        },
+      },
+    ],
+  },
+};
